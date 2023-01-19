ef5205920bb76bbcaadadc5b8093388c27f737c2
Pau <53918859+Jongsuu@users.noreply.github.com>
2023-01-19 12:50:20 +0100

    Added nightfox theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,6 +2,41 @@ require('rose-pine').setup({
 	disable_background = true
 })
 
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = false,    -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = false,   -- Non focused panes set to alternative background
+    module_default = true,  -- Default enable value for modules
+    styles = {              -- Style to be applied to different syntax groups
+      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "italic,bold",
+      functions = "bold",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "italic,bold",
+      variables = "bold",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+})
+
 function ColorMyPencils(color) 
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
@@ -10,4 +45,4 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
 end
 
-ColorMyPencils()
+ColorMyPencils('nightfox')
