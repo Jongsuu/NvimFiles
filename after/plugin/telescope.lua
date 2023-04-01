ad6919b5fe999f540d3879a49bd219842bb16926
Pau <byprokiller2@gmail.com>
2023-04-01 14:23:09 +0200

    Added new remaps for git integrations and opening files in new split

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- espai + pf
-vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- Control + p
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+local opts = { noremap = true, silent = true, expr = false }
+
+vim.keymap.set('n', '<leader>pvs', ':vs | Telescope find_files<CR>', opts)
