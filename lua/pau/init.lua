f7d354207f2d2b5a79f5228ff34d92dd78f2e6f1
Pau <byprokiller2@gmail.com>
2023-04-04 15:17:38 +0200

    Autocommand to remove whitespaces

--- lua/pau/init.lua
+++ lua/pau/init.lua
@@ -1,3 +1,14 @@
 require("pau.remap")
 require("pau.set")
 require("pau.packer")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local PauGroup = augroup('Pau', {})
+
+-- Autocommand to remove all trailing whitespaces at the end of the line when writting the file
+autocmd({"BufWritePre"}, {
+    group = PauGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
