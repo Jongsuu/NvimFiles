40ddb40fbe025da11cb93e7c72b1160c711c0a20
Pau <byprokiller2@gmail.com>
2023-04-15 12:57:06 +0200

    Fixed grep string with telescope + documenting remaps.lua

--- lua/pau/remap.lua
+++ lua/pau/remap.lua
@@ -1,3 +1,4 @@
+-- <leader> is Space
 vim.g.mapleader = " "
 
 -- Open file explorer
@@ -7,29 +8,31 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Moves the line below up
+-- Moves the line below up and don't move the cursor
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- Navigate half page up or down and center cursor
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
--- Goes to next ocurrence of the pattern and centers the cursor
+-- Goes to next ocurrence of the pattern and center cursor
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Pastes but doesn't copy the selection you pasted to
+-- Pastes on the selection but doesn't copy the selection you pasted to
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Copies to the system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- Deletes the selection but doesn't copy the selection you pasted to
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Makes Ctrl + c act the same way Esc acts
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- unbind Q
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
